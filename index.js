3eed5648f71bb7bb5204d9d5a81e4c5e21d2c596
WendellOliver <wendellofmendes@gmail.com>
2021-06-05 08:40:36 -0300

    alteracoes

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const users = document.querySelector('.user');
-var data = null;
+const cards = document.querySelector('.card');
 
-var request = new XMLHttpRequest();
-request.open("GET", 'https://randomuser.me/api/?seed=javascript&results=1&nat=BR&noinfo', true);
+url = 'https://randomuser.me/api/?seed=javascript&results=3&nat=BR&noinfo';
 
-request.onload = function () {
-    data=json.parse(this.response)
-
-}
-
-const p=document.createElement("p");
-const html=` 
-<p> ${data.results.name.first}</p>
-
-`;
-
-p.innerHTML=html;
-
-users.appendChild=p;
+fetch(url) // método global que fornece uma maneira fácil e lógica de buscar recursos de forma assíncrona na rede.
+    .then((resp) => resp.json()) //retorna uma promessa contendo a resposta (um Responseobjeto).
+    .then((data) => { //Os dados JSON aqui serão processados. 
+        let authors = data.results; // data.results contem o array de objetos retornado pela API
+        return authors.forEach(element => {
+            
+        
+        const html = `
+                        <p>${element.name.first}</p> 
+                    `; // pegamos aqui a única opção que está sendo retornada - posição do array 0
+        const div = document.createElement('li');
+        div.innerHTML = html;
+         cards.appendChild(div);
+        });
+        console.log(authors);
+    }).catch(function(error) { // caso a API retorne erro, capturamos com o catch
+        console.log(error);
+    });
